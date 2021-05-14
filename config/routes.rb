826c058e61be6e2c254076207b20447e9f8f2db7
Camille Morin <dewavrin.camille@gmail.com>
2021-05-14 08:54:11 +0200

    MVC rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :new, :show, :create ] do
+  # get '/lists', to: 'lists#index'
+  # get '/lists/new', to: 'lists#new'
+  # get '/lists/:id', to: 'lists#show'
+  # post '/lists', to: 'lists#create', as: :list
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [ :destroy ]
 end
